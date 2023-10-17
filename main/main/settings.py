f311d7496369cee1a577f6ca056b42981a7cc39b
Ashok <bayes.ashok@gmail.com>
2023-10-17 11:43:15 +0545

    created model

--- main/main/settings.py
+++ main/main/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'myApp.apps.MyappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- main/myApp/views.py
+++ main/myApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render 
 # render is for template
 from django.http import HttpResponse
+from myApp.models import Contact
 
 # Create your views here.
 def index(request):
@@ -15,4 +16,12 @@ def about(request):
 def services(request):
     return render(request,"services.html")
 def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        desc=request.POST.get('desc')
+        contact=Contact(name=name, email=email, desc=desc)
+        contact.save()
+
+    
     return render(request,"contact.html")
